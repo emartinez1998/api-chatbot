675ef78f61e8ffeeaaa53b535a2d4f81daa30877
Enrique <osielenrique.martinez@set.edu.mx>
2025-03-24 15:32:06 -0600

    Metodo publico

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import chatbotGet, chatbotPost  # Importa las funciones de vista
+from .views import chatbotGet, chatbotPost, chatbotGetPublic  # Importa las funciones de vista
 
 urlpatterns = [
     path('chatbot/get/', chatbotGet, name='chatbot-get'),  # Endpoint GET
     path('chatbot/post/', chatbotPost, name='chatbot-post'),  # Endpoint POST
+    path('chatbot/public/', chatbotGetPublic, name='chatbot-public'),  # Endpoint POST
 ]
 
--- chatbot/views.py
+++ chatbot/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework_api_key.permissions import HasAPIKey
+from rest_framework.permissions import AllowAny 
 
 @api_view(['GET'])
 @permission_classes([HasAPIKey])
@@ -17,3 +18,11 @@ def chatbotPost(request):
     
     mensaje = f"El parámetro 'dato' ha sido recibido: {dato}"
     return Response({"mensaje": mensaje}, status=200)
+
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])  # Permiso para acceso público
+def chatbotGetPublic(request):
+    data = {"definicion": "Liiffe es una plataforma de viajes que ofrece itinerarios detallados de 3 días diseñados por lugareños para una experiencia auténtica."}
+    return Response(data, status=200)  # Se devuelve JSON con código 200 OK
