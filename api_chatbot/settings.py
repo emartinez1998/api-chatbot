5e63aac81de28b72e54eb36547a0e58604e63193
Edilmerio <edilmerio.martinez@gmail.com>
2025-05-27 12:08:53 -0400

    fix settings

--- api_chatbot/settings.py
+++ api_chatbot/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'api_chatbot.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db' / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
